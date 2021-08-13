c2271afcf1fc864b770edbb743afef590a767a52
Ankit Yadav <ankitprnl@gmail.com>
2021-08-13 16:09:44 +0530

    migrated python backend to node

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const router = require('./routes/routes');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.set('view engine', 'hbs');
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
---
+++ src/routes/routes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  await res.redirect('https://github.com/ankityadavhere');
+});
+
+router.get('/gh', async (req, res) => {
+  await res.redirect('https://github.com/ankityadavhere');
+});
+
+router.get('/repo', async (req, res) => {
+  await res.redirect('https://github.com/ankityadavhere/url-shortener');
+});
+
+router.get('/nd', async (req, res) => {
+  await res.redirect('https://ankityadavhere.github.io/NotoDesk');
+});
+
+router.get('/ndc', async (req, res) => {
+  await res.redirect('https://github.com/ankityadavhere/NotoDesk');
+});
+
+router.get('/wc', async (req, res) => {
+  await res.redirect('https://whatsapp-node-clone.herokuapp.com');
+});
+
+router.get('/wcc', async (req, res) => {
+  await res.redirect('https://github.com/ankityadavhere/whatsapp-clone');
+});
+
+router.get('*', async (req, res) => {
+  await res.render('404');
+});
+
+module.exports = router;
