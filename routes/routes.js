d522a5c9a3eb622eedf0710d4e0e637036d3512b
Ankit Yadav <ankitprnl@gmail.com>
2021-08-22 19:30:12 +0530

    last test

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ const ua = require('universal-analytics');
 const visitor = ua(process.env.GA_ACCID);
 
 router.get('/run', async (req, res) => {
-  await visitor.pageview('/run').send();
+  visitor.pageview('/run').send();
   await res.redirect('https://github.com');
 });
 
